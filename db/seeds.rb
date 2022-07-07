d95b45d551818cd436b31e82b0c6b7408b61b862
hamish liu <hamish.mc.liu@gmail.com>
2022-07-07 20:00:23 +0900

    basic set up done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create 4 flats and seed them into the database
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Melbourne low roof flat',
+  address: '10 Clifton river Melbourne W9 1DT',
+  description: 'not bad if i do say so myself',
+  price_per_night: 56,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'tokyo high roof flat',
+  address: '10 Clifton river Tokyo W9 1DT',
+  description: '英語がまだ書けない',
+  price_per_night: 23,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'pairs high roof flat',
+  address: '10 Clifton river paris W9 1DT',
+  description: 'le best flat in all of pari',
+  price_per_night: 88,
+  number_of_guests: 2
+)
